fc1eb9dd5422ed2e73533f25441187e2aa32a482
subhashis2204 <subhashispaul2204@gmail.com>
2022-10-27 12:50:48 +0530

    added stackoverflow, github and google search reasults panel

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const path = require('path');
-const { youtube } = require('./utility');
+const { youtube, stackoverflow, github, googleSearch } = require('./utility');
 const app = express();
 const port = 3000
 const results = require('./data')
 
 require('dotenv').config({ path: './TEST.env' })
 
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -19,7 +20,12 @@ app.get("/", (req, res) => {
 
 app.get("/search", async(req, res) => {
     const searchQuery = req.query.search_query
+
     await youtube(searchQuery);
+    await stackoverflow(searchQuery)
+    await github(searchQuery)
+    await googleSearch(searchQuery)
+
     res.render('youtube', { results })
 })
 
--- utility.js
+++ utility.js
@@ -1,4 +1,5 @@
 let results = require('./data')
+const axios = require('axios')
 
 module.exports.youtube = async(query_parameter) => {
     const { google } = require('googleapis')
@@ -23,14 +24,50 @@ module.exports.youtube = async(query_parameter) => {
     results.youtube = youtubeData
 }
 
-async function stackoverflow(query_parameter) {
+module.exports.stackoverflow = async(query_parameter) => {
+    const baseUrl = `https://api.stackexchange.com/2.3/search?pagesize=10&order=desc&sort=activity&intitle=${query_parameter}&site=stackoverflow`
+    const response = await axios.get(baseUrl)
 
+    const searchResults = response.data.items
+    const stackoverflowData = searchResults.map(searchResult => {
+        return {
+            title: searchResult.title,
+            url: searchResult.link,
+            tags: searchResult.tags
+        }
+    })
+    results.stackoverflow = stackoverflowData
 }
 
-async function googleSearch(query_parameter) {
+module.exports.googleSearch = async(query_parameter) => {
+    const baseUrl = "https://api.scaleserp.com"
+    const searchUrl = `${baseUrl}/search?api_key=${process.env.API_KEY_GS}&q=${query_parameter}`
 
+    const response = await axios.get(searchUrl)
+    const searchResults = response.data.organic_results
+
+    const googleSearchData = searchResults.map(searchResult => {
+        return {
+            title: searchResult.title,
+            url: searchResult.link,
+            description: searchResult.snippet
+        }
+    })
+    results.googleSearch = googleSearchData
 }
 
-async function github(query_parameter) {
+module.exports.github = async(query_parameter) => {
+    const baseUrl = `https://api.github.com/search`
+    const searchUrl = `${baseUrl}/repositories?q=${query_parameter}`
+    const response = await axios.get(searchUrl)
 
+    const searchResults = response.data.items
+    const githubData = searchResults.map(searchResult => {
+        return {
+            title: searchResult.full_name,
+            url: searchResult.html_url,
+            description: searchResult.description
+        }
+    })
+    results.github = githubData
 }
